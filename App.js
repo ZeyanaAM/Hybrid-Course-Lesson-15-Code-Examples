5e286ffd788cabdbf8b25c4d022ce27ea1510485
ZeyanaAM <zeyana.a@berkeley.edu>
2021-02-01 13:42:08 +0300

    Animation code examples

--- App.js
+++ App.js
@@ -1,14 +1,116 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Animated, Button } from 'react-native';
 
-export default function App() {
+const FadeAnimationExample = () => {
+  const fade = new Animated.Value(0);
+  // console.log('fade value: ', fade);
+
+  const fadeIn = Animated.timing(fade, {
+    toValue: 1,
+    duration: 2000,
+    useNativeDriver: true,
+  });
+
+  const fadeOut = Animated.timing(fade, {
+    toValue: 0,
+    duration: 2000,
+    useNativeDriver: true,
+  });
+
+  // fadeIn.start(() => console.log('animation complete'));
+
+  const fadeInAndOut = Animated.sequence([fadeIn, fadeOut]);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Animated.View
+        style={{
+          backgroundColor: 'orange',
+          width: 100,
+          height: 100,
+          opacity: fade,
+        }}
+      />
+      <Button title="Fade In Animation" onPress={() => fadeIn.start()} />
+      <Button title="Fade Out Animation" onPress={() => fadeOut.start()} />
+      <Button
+        title="Fade In and Out Animation"
+        onPress={() => fadeIn.start(() => fadeOut.start())}
+      />
+      <Button
+        title="Fade In and Out Animation using Sequence"
+        onPress={() => fadeInAndOut.start()}
+      />
     </View>
   );
+};
+
+const TransformExample = () => {
+  const scale = new Animated.Value(1);
+
+  const increaseSize = Animated.timing(scale, {
+    toValue: 1.5,
+    duration: 1000,
+    useNativeDriver: true,
+  });
+
+  const springOut = Animated.spring(scale, {
+    toValue: 1.5,
+    friction: 4,
+    // bounciness: 5,
+    useNativeDriver: true,
+  });
+
+  const decreaseSize = Animated.timing(scale, {
+    toValue: 1,
+    duration: 1000,
+    useNativeDriver: true,
+  });
+
+  const rotate = new Animated.Value(0);
+  const rotateBox = Animated.timing(rotate, {
+    toValue: 1,
+    duration: 1000,
+    useNativeDriver: true,
+  });
+
+  // rotateBox.start();
+
+  const degrees = rotate.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['0deg', '180deg'],
+  });
+
+  return (
+    <View style={styles.container}>
+      <Animated.View
+        style={{
+          backgroundColor: 'orange',
+          width: 100,
+          height: 100,
+          transform: [
+            {
+              scale: scale,
+              rotate: degrees,
+            },
+          ],
+        }}
+      />
+      <Button
+        title="Scale Out Animation"
+        onPress={() => increaseSize.start()}
+      />
+      <Button title="Scale In Animation" onPress={() => decreaseSize.start()} />
+      <Button title="Spring Out Animation" onPress={() => springOut.start()} />
+      <Button title="Rotate" onPress={() => rotateBox.start()} />
+    </View>
+  );
+};
+
+export default function App() {
+  return <TransformExample />;
+  // return <FadeAnimationExample />;
 }
 
 const styles = StyleSheet.create({
